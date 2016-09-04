845c43c391a150c965a1ba507c310b5c5e902e4f
lynong <lynong@hotmail.com>
2016-09-04 08:31:12 -0400

    Update and rename acortador2.py to acortador.py

--- acortador.py
+++ acortador.py
@@ -1,6 +1,6 @@
 import requests , json
- 
-Api_key = "be8ff7aaa4bbfd509e341209646e390bf846e8ef"
+#Se ingresa la api key obtenida en https://adsnet.info
+Api_key = "ApiKey"
 base = "https://adsnet.info/api/?api="
 Base1 = "&url="
 link = input('ingresa link a acortar : ')
