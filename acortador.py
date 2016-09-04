3120e1a382c9f3e00a673ca0989b27d0f9ba0def
lynong <lynong@hotmail.com>
2016-09-04 08:59:20 -0400

    Update acortador.py

--- acortador.py
+++ acortador.py
@@ -8,7 +8,7 @@ link = input('ingresa link a acortar : ')
 #Se construye la url final con las bases y l Api Key
 url_final= base + Api_key + Base1 + link
 """Se envia una peticion a la url final a cambio se obtiene un documento JSON y en este caso especificamos que queremos
-el valor shortenedUrl que es la url ya finalmente acortada""""
+el valor shortenedUrl que es la url ya finalmente acortada"""
 r = requests.get(url_final).json()['shortenedUrl']
 #Finalmente se imprime en pantalla la url ya acortada
 print (r)
